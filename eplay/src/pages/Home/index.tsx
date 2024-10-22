a2809aceddc8017506b16fe850181430f17b73bd
Reinaldo <reinaldo18junior@gmail.com>
2024-10-22 15:49:49 -0300

    Criando página de categoria e routes.tsx

---
+++ eplay/src/pages/Home/index.tsx
@@ -0,0 +1,104 @@
+import Banner from '../../components/Banner'
+import ProductsList from '../../components/ProductsList'
+import Game from '../../models/Game'
+
+import resident from '../../assets/images/resident.png'
+import diablo from '../../assets/images/diablo.png'
+import zelda from '../../assets/images/zelda.png'
+import starWars from '../../assets/images/star_wars.png'
+
+const promocoes: Game[] = [
+  {
+    id: 1,
+    category: 'Ação',
+    description:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima quisquam rem harum sit accusantium iste beatae, nesciunt adipisci eaque',
+    title: 'Resident Evil 4',
+    system: 'Windows',
+    infos: ['10%', 'R$ 250,00'],
+    image: resident
+  },
+  {
+    id: 2,
+    category: 'Ação',
+    description:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima quisquam rem harum sit accusantium iste beatae, nesciunt adipisci eaque',
+    title: 'Resident Evil 4',
+    system: 'PS5',
+    infos: ['5%', 'R$ 290,00'],
+    image: resident
+  },
+  {
+    id: 3,
+    category: 'Ação',
+    description:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima quisquam rem harum sit accusantium iste beatae, nesciunt adipisci eaque',
+    title: 'Resident Evil 4',
+    system: 'Windows',
+    infos: ['10%', 'R$ 250,00'],
+    image: resident
+  },
+  {
+    id: 4,
+    category: 'Ação',
+    description:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima quisquam rem harum sit accusantium iste beatae, nesciunt adipisci eaque',
+    title: 'Resident Evil 4',
+    system: 'Windows',
+    infos: ['10%', 'R$ 250,00'],
+    image: resident
+  }
+]
+
+const emBreve: Game[] = [
+  {
+    id: 5,
+    category: 'RPG',
+    description:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima quisquam rem harum sit accusantium iste beatae, nesciunt adipisci eaque',
+    title: 'Diablo 4',
+    system: 'Windows',
+    infos: ['17/05'],
+    image: diablo
+  },
+  {
+    id: 6,
+    category: 'RPG',
+    description:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima quisquam rem harum sit accusantium iste beatae, nesciunt adipisci eaque',
+    title: 'Zelda',
+    system: 'Windows',
+    infos: ['17/05'],
+    image: zelda
+  },
+  {
+    id: 7,
+    category: 'RPG',
+    description:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima quisquam rem harum sit accusantium iste beatae, nesciunt adipisci eaque',
+    title: 'Star Wars',
+    system: 'Windows',
+    infos: ['17/05'],
+    image: starWars
+  },
+  {
+    id: 8,
+    category: 'RPG',
+    description:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima quisquam rem harum sit accusantium iste beatae, nesciunt adipisci eaque',
+    title: 'Resident Evil 4',
+    system: 'Nintendo Switch',
+    infos: ['17/05'],
+    image: resident
+  }
+]
+
+const Home = () => (
+  <>
+    <Banner />
+    <ProductsList games={promocoes} title="Promoções" background="gray" />
+    <ProductsList games={emBreve} title="Em Breve" background="black" />
+  </>
+)
+
+export default Home
