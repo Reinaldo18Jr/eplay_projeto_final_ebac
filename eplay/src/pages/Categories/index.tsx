a0e6a6ab1ddb9c69c5b5351e32a55a8edfb9241d
Reinaldo <reinaldo18junior@gmail.com>
2024-11-21 14:58:29 -0300

    Integrando a API de produtos

--- eplay/src/pages/Categories/index.tsx
+++ eplay/src/pages/Categories/index.tsx
@@ -1,104 +1,49 @@
+import { useEffect, useState } from 'react'
+import { Game } from '../Home'
 import ProductsList from '../../components/ProductsList'
-import Game from '../../models/Game'
 
-import resident from '../../assets/images/resident.png'
-import diablo from '../../assets/images/diablo.png'
-import zelda from '../../assets/images/zelda.png'
-import starWars from '../../assets/images/star_wars.png'
+const Categories = () => {
+  const [gamesAcao, setGamesAcao] = useState<Game[]>([])
+  const [gamesEsporte, setGamesEsporte] = useState<Game[]>([])
+  const [gamesSimulacao, setGamesSimulacao] = useState<Game[]>([])
+  const [gamesLuta, setGamesLuta] = useState<Game[]>([])
+  const [gamesRpg, setGamesRpg] = useState<Game[]>([])
 
-const promocoes: Game[] = [
-  {
-    id: 1,
-    category: 'Ação',
-    description:
-      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima quisquam rem harum sit accusantium iste beatae, nesciunt adipisci eaque',
-    title: 'Resident Evil 4',
-    system: 'Windows',
-    infos: ['10%', 'R$ 250,00'],
-    image: resident
-  },
-  {
-    id: 2,
-    category: 'Ação',
-    description:
-      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima quisquam rem harum sit accusantium iste beatae, nesciunt adipisci eaque',
-    title: 'Resident Evil 4',
-    system: 'PS5',
-    infos: ['5%', 'R$ 290,00'],
-    image: resident
-  },
-  {
-    id: 3,
-    category: 'Ação',
-    description:
-      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima quisquam rem harum sit accusantium iste beatae, nesciunt adipisci eaque',
-    title: 'Resident Evil 4',
-    system: 'Windows',
-    infos: ['10%', 'R$ 250,00'],
-    image: resident
-  },
-  {
-    id: 4,
-    category: 'Ação',
-    description:
-      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima quisquam rem harum sit accusantium iste beatae, nesciunt adipisci eaque',
-    title: 'Resident Evil 4',
-    system: 'Windows',
-    infos: ['10%', 'R$ 250,00'],
-    image: resident
-  }
-]
+  useEffect(() => {
+    fetch('https://fake-api-tau.vercel.app/api/eplay/acao')
+      .then((resp) => resp.json())
+      .then((resp) => setGamesAcao(resp))
 
-const emBreve: Game[] = [
-  {
-    id: 5,
-    category: 'RPG',
-    description:
-      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima quisquam rem harum sit accusantium iste beatae, nesciunt adipisci eaque',
-    title: 'Diablo 4',
-    system: 'Windows',
-    infos: ['17/05'],
-    image: diablo
-  },
-  {
-    id: 6,
-    category: 'RPG',
-    description:
-      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima quisquam rem harum sit accusantium iste beatae, nesciunt adipisci eaque',
-    title: 'Zelda',
-    system: 'Windows',
-    infos: ['17/05'],
-    image: zelda
-  },
-  {
-    id: 7,
-    category: 'RPG',
-    description:
-      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima quisquam rem harum sit accusantium iste beatae, nesciunt adipisci eaque',
-    title: 'Star Wars',
-    system: 'Windows',
-    infos: ['17/05'],
-    image: starWars
-  },
-  {
-    id: 8,
-    category: 'RPG',
-    description:
-      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima quisquam rem harum sit accusantium iste beatae, nesciunt adipisci eaque',
-    title: 'Resident Evil 4',
-    system: 'Nintendo Switch',
-    infos: ['17/05'],
-    image: resident
-  }
-]
+    fetch('https://fake-api-tau.vercel.app/api/eplay/esportes')
+      .then((resp) => resp.json())
+      .then((resp) => setGamesEsporte(resp))
 
-const Categories = () => (
-  <>
-    <ProductsList games={promocoes} title="RPG" background="gray" />
-    <ProductsList games={emBreve} title="Ação" background="black" />
-    <ProductsList games={promocoes} title="Aventura" background="gray" />
-    <ProductsList games={emBreve} title="FPS" background="black" />
-  </>
-)
+    fetch('https://fake-api-tau.vercel.app/api/eplay/simulacao')
+      .then((resp) => resp.json())
+      .then((resp) => setGamesSimulacao(resp))
+
+    fetch('https://fake-api-tau.vercel.app/api/eplay/luta')
+      .then((resp) => resp.json())
+      .then((resp) => setGamesLuta(resp))
+
+    fetch('https://fake-api-tau.vercel.app/api/eplay/rpg')
+      .then((resp) => resp.json())
+      .then((resp) => setGamesRpg(resp))
+  }, [])
+
+  return (
+    <>
+      <ProductsList games={gamesAcao} title="Ação" background="black" />
+      <ProductsList games={gamesEsporte} title="Esporte" background="gray" />
+      <ProductsList
+        games={gamesSimulacao}
+        title="Simulação"
+        background="black"
+      />
+      <ProductsList games={gamesLuta} title="Luta" background="gray" />
+      <ProductsList games={gamesRpg} title="RPG" background="black" />
+    </>
+  )
+}
 
 export default Categories
--- eplay/src/pages/Home/index.tsx
+++ eplay/src/pages/Home/index.tsx
@@ -1,104 +1,58 @@
+import { useEffect, useState } from 'react'
+
 import Banner from '../../components/Banner'
 import ProductsList from '../../components/ProductsList'
-import Game from '../../models/Game'
 
-import resident from '../../assets/images/resident.png'
-import diablo from '../../assets/images/diablo.png'
-import zelda from '../../assets/images/zelda.png'
-import starWars from '../../assets/images/star_wars.png'
+export interface GalleryItem {
+  type: 'image' | 'video'
+  url: string
+}
 
-const promocoes: Game[] = [
-  {
-    id: 1,
-    category: 'Ação',
-    description:
-      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima quisquam rem harum sit accusantium iste beatae, nesciunt adipisci eaque',
-    title: 'Resident Evil 4',
-    system: 'Windows',
-    infos: ['10%', 'R$ 250,00'],
-    image: resident
-  },
-  {
-    id: 2,
-    category: 'Ação',
-    description:
-      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima quisquam rem harum sit accusantium iste beatae, nesciunt adipisci eaque',
-    title: 'Resident Evil 4',
-    system: 'PS5',
-    infos: ['5%', 'R$ 290,00'],
-    image: resident
-  },
-  {
-    id: 3,
-    category: 'Ação',
-    description:
-      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima quisquam rem harum sit accusantium iste beatae, nesciunt adipisci eaque',
-    title: 'Resident Evil 4',
-    system: 'Windows',
-    infos: ['10%', 'R$ 250,00'],
-    image: resident
-  },
-  {
-    id: 4,
-    category: 'Ação',
-    description:
-      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima quisquam rem harum sit accusantium iste beatae, nesciunt adipisci eaque',
-    title: 'Resident Evil 4',
-    system: 'Windows',
-    infos: ['10%', 'R$ 250,00'],
-    image: resident
+export type Game = {
+  id: number
+  name: string
+  description: string
+  release_date?: string
+  prices: {
+    discount?: number
+    old?: number
+    current?: number
   }
-]
-
-const emBreve: Game[] = [
-  {
-    id: 5,
-    category: 'RPG',
-    description:
-      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima quisquam rem harum sit accusantium iste beatae, nesciunt adipisci eaque',
-    title: 'Diablo 4',
-    system: 'Windows',
-    infos: ['17/05'],
-    image: diablo
-  },
-  {
-    id: 6,
-    category: 'RPG',
-    description:
-      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima quisquam rem harum sit accusantium iste beatae, nesciunt adipisci eaque',
-    title: 'Zelda',
-    system: 'Windows',
-    infos: ['17/05'],
-    image: zelda
-  },
-  {
-    id: 7,
-    category: 'RPG',
-    description:
-      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima quisquam rem harum sit accusantium iste beatae, nesciunt adipisci eaque',
-    title: 'Star Wars',
-    system: 'Windows',
-    infos: ['17/05'],
-    image: starWars
-  },
-  {
-    id: 8,
-    category: 'RPG',
-    description:
-      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima quisquam rem harum sit accusantium iste beatae, nesciunt adipisci eaque',
-    title: 'Resident Evil 4',
-    system: 'Nintendo Switch',
-    infos: ['17/05'],
-    image: resident
+  details: {
+    category: string
+    system: string
+    developer: string
+    publisher: string
+    languages: string[]
   }
-]
+  media: {
+    thumbnail: string
+    cover: string
+    gallery: GalleryItem[]
+  }
+}
 
-const Home = () => (
-  <>
-    <Banner />
-    <ProductsList games={promocoes} title="Promoções" background="gray" />
-    <ProductsList games={emBreve} title="Em Breve" background="black" />
-  </>
-)
+const Home = () => {
+  const [promocoes, setPromocoes] = useState<Game[]>([])
+  const [emBreve, setEmBreve] = useState<Game[]>([])
+
+  useEffect(() => {
+    fetch('https://fake-api-tau.vercel.app/api/eplay/promocoes')
+      .then((resp) => resp.json())
+      .then((resp) => setPromocoes(resp))
+
+    fetch('https://fake-api-tau.vercel.app/api/eplay/em-breve')
+      .then((resp) => resp.json())
+      .then((resp) => setEmBreve(resp))
+  }, [])
+
+  return (
+    <>
+      <Banner />
+      <ProductsList games={promocoes} title="Promoções" background="gray" />
+      <ProductsList games={emBreve} title="Em Breve" background="black" />
+    </>
+  )
+}
 
 export default Home
